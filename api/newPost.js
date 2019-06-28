36e2c97291089e91dc617515032e47fb4d2e8d2a
necr0sys <felixtineo05@gmail.com>
2019-06-28 18:20:09 -0400

    add post

---
+++ api/newPost.js
@@ -0,0 +1,21 @@
+const app = require('./util/app');
+const mongooseConnect = require('./util/mongoose');
+const UserSchema = require('../models/user');
+
+[...mongooseConnect];
+
+app.post('*', async (req, res) => {
+  const { id, text, date } = req.body;
+  const newPost = {
+    text,
+    date,
+  };
+  const resp = await UserSchema.findByIdAndUpdate(id, {$push: {posts: newPost}});
+  if (resp) {
+    res.status(200);
+  } else {
+    res.status(501);
+  }
+});
+
+module.exports = app;
