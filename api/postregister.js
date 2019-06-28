aa58776c13726c47cd3c53ef038ba847e36d2571
necr0sys <felixtineo05@gmail.com>
2019-06-28 10:01:06 -0400

    fix final de postregistro, creo..

--- api/postregister.js
+++ api/postregister.js
@@ -8,7 +8,7 @@ app.post('*', async (req, res) => {
   const { id, img } = req.body;
   const isOk = await UserSchema.findByIdAndUpdate(id, { perfilImg: img });
   if (isOk) {
-    res.status.send(id);
+    res.status(200).send(id);
   } else {
     res.status(501).send(id);
   }
