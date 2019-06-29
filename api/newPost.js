31258ab468e6b93a508e228dc84fe8870154b1f4
necr0sys <felixtineo05@gmail.com>
2019-06-28 21:30:05 -0400

    yaa

--- api/newPost.js
+++ api/newPost.js
@@ -5,18 +5,16 @@ const UserSchema = require('../models/user');
 [...mongooseConnect];
 
 app.post('*', async (req, res) => {
-  const reqId = req.body.id;
-  const reqText = req.body.text;
-  const reqDate = req.body.reqDate;
+  const { id, text, date } = req.body;
   const newPost = {
-    text: reqText,
-    date: reqDate,
-  }
-  const resp = await UserSchema.findByIdAndUpdate(reqId, { $push: { posts: newPost } });
+    text,
+    date,
+  };
+  const resp = await UserSchema.findByIdAndUpdate(id, { $push: { posts: newPost } });
   if (resp) {
-    res.status(200);
+    res.status(200).send();
   } else {
-    res.status(501);
+    res.status(501).send();
   }
 });
 
